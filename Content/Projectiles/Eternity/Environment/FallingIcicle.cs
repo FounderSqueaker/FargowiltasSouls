5b22f153f4c23d16e4f7501e08d3a16c87f7313b
Habble53 <87115317+Habble53@users.noreply.github.com>
2025-11-11 11:35:54 +0300

    Tundra icicle check unactuated tile for top

--- Content/Projectiles/Eternity/Environment/FallingIcicle.cs
+++ Content/Projectiles/Eternity/Environment/FallingIcicle.cs
@@ -77,7 +77,7 @@ namespace FargowiltasSouls.Content.Projectiles.Eternity.Environment
                 }
             }
 
-            bool hasTop = Main.tile[new Vector2(Projectile.position.X, Projectile.position.Y - 8f).ToTileCoordinates16()].HasTile ? true : false; // TODO: Make this better later so that it makes icicle fall instead
+            bool hasTop = Main.tile[new Vector2(Projectile.position.X, Projectile.position.Y - 8f).ToTileCoordinates16()].HasUnactuatedTile ? true : false; // TODO: Make this better later so that it makes icicle fall instead
 
             if (target == null || Projectile.Center.Distance(target.Center) > 500 || (Projectile.ai[0] == 0 && !hasTop))
             {
